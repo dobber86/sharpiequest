ba45dfe852dc24f90727f57667d55932257ec45e
dobber86 <35963693+dobber86@users.noreply.github.com>
2018-04-11 14:27:10 +0200

    Add files via upload

--- public/js/services/playerStats.js
+++ public/js/services/playerStats.js
@@ -1,13 +1,20 @@
 app.service('playerStats', function() {
     
     var username = "";
+    var xp = 0;
 
-    this.saveUsername=function(x) {
-        username = x;
+    this.saveUsername = function(x, y) {
+        username = x,
+        xp = y
     }
 
-    this.getUsername=function() {
+    this.getUsername = function() {
         return username;
     }
-  
-  });
+
+    this.getXp = function() {
+        return xp;
+    }
+    
+
+});
