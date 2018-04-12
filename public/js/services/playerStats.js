c872c046b923f6185157602bea5e3a050b690c26
Galfaren <38285044+Galfaren@users.noreply.github.com>
2018-04-12 11:55:36 +0200

    Add files via upload

--- public/js/services/playerStats.js
+++ public/js/services/playerStats.js
@@ -1,40 +1,78 @@
 app.service('playerStats', function() {
     
     var username = "",
-        hp = 10,
-        maxhp = 10,
-        mp = 5,
-        maxmp = 5,
-        power = 1,
-        resistance = 1,
-        accuracy = 8,
-        insight = 1,
-        specialpower = 1,
+        hp = 15,
+        maxhp = 15,
+        mp = 10,
+        maxmp = 10,
+        power = 3,
+        resistance = 13,
+        accuracy = 85,
+        insight = 85,
+        specialpower = 3,
         xp = 1,
         level = 1;
 
-    this.saveUsername = function(user, hp, mhp, mp, mmp, pow, res, acc, ins, spe, xp, lvl) {
-        username = user,
-        hp = hp,
-        maxhp = mhp,
+    this.saveUsername = function(dbusername, dbhp, dbmhp, dbmp, dbmmp, dbpow, dbres, dbacc, dbins, dbspe, dbxp, dblvl) {
+        username = dbusername,
+        hp = dbhp,
+        maxhp = dbmhp,
         mp = mp,
-        maxmp = mmp,
-        power = pow,
-        resistance = res,
-        accuracy = acc,
-        insight = ins,
-        specialpower = spe,
-        xp = xp,
-        level = lvl
+        maxmp = dbmmp,
+        power = dbpow,
+        resistance = dbres,
+        accuracy = dbacc,
+        insight = dbins,
+        specialpower = dbspe,
+        xp = dbxp,
+        level = dblvl
     }
 
     this.getUsername = function() {
         return username;
     }
 
+    this.getHp = function() {
+        return hp;
+    }
+
+    this.getMaxHp = function() {
+        return maxhp;
+    }
+
+    this.getMp = function() {
+        return mp;
+    }
+
+    this.getMaxMp = function() {
+        return maxmp;
+    }
+
+    this.getPower = function() {
+        return power;
+    }
+
+    this.getResistance = function() {
+        return resistance;
+    }
+
+    this.getAccuracy = function() {
+        return accuracy;
+    }
+
+    this.getInsight = function() {
+        return insight;
+    }
+
+    this.getSpecialPower = function() {
+        return specialpower;
+    }
+
     this.getXp = function() {
         return xp;
     }
-    
 
+    this.getLevel = function() {
+        return level;
+    }
 });
