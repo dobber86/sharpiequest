4fc617d6a1d7e3f7cb25e9ca3a493b9747116442
dobber86 <35963693+dobber86@users.noreply.github.com>
2018-04-13 11:46:10 +0200

    Add files via upload

--- public/js/controllers/combatController.js
+++ public/js/controllers/combatController.js
@@ -23,6 +23,7 @@ app.controller('combatController', function($scope, $location, $timeout, monster
   $scope.playerResistance = $scope.playerMaxResistance;
   $scope.playerInsight = playerStats.getInsight();
   $scope.playerLevel = playerStats.getLevel();
+  $scope.playerMoney = playerStats.getMoney();
   $scope.playerImage = "img/char/warrior/idle.gif";
 
   // Player bar size
@@ -324,12 +325,14 @@ app.controller('combatController', function($scope, $location, $timeout, monster
       if ($scope.enemyAlive === false) {
         $scope.buttonLock = true;
         $scope.combatLog = "The "+$scope.enemyName+" is dead!";
-        $scope.characterQuote = "Victory!"
+        $scope.characterQuote = "Victory!";
+        $scope.playerVictory();
       }
       if ($scope.playerAlive === false) {
         $scope.buttonLock = true; 
         $scope.combatLog = "You are dead, oh dear!";
         $scope.characterQuote = "Urgh..."
+        $scope.playerDead();
       }
     }, 2000);
 
@@ -347,4 +350,24 @@ app.controller('combatController', function($scope, $location, $timeout, monster
 
     $scope.turnCount++;
   }
+
+  $scope.playerVictory = function() {
+    $scope.playerLevel += 1;
+    playerStats.saveCombatUpdate(
+      $scope.playerCurrentHP,
+      $scope.playerCurrentMP,
+      $scope.playerXp,
+      $scope.playerLevel
+    );
+  }
+
+  $scope.playerDead = function() {
+    playerStats.saveCombatUpdate(
+      $scope.playerCurrentHP,
+      $scope.playerCurrentMP,
+      $scope.playerXp,
+      $scope.playerLevel
+    );
+  }
+  
 });
